331eae69f3341ac2d1ce4a6a101d7185ef2fba4d
AnGgIt886 <jarbull86@gmail.com>
2025-10-03 01:33:04 +0700

    CI: Bump to 03102025 build
    Clang version: M•R•T clang version 22.0.0git (https://github.com/llvm/llvm-project 5bbf72400cfeb8f4e205e9ff1c98d34d2997796c)
    LLVM repo commit: [X86] Add bytecode / -fexperimental-new-constant-interpreter VPMADD52 intrinsics test coverage (#161684)
    Link: https://github.com/llvm/llvm-project/commit/5bbf72400cfeb8f4e205e9ff1c98d34d2997796c
    
    Change-Id: Ia5c8d97ade9799ab38b0901670ffad082a5d40be
    Signed-off-by: AnGgIt886 <jarbull86@gmail.com>

---
+++ get_latest_url.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+LATEST_URL_GZ=https://github.com/MRT-project/mrt_clang/releases/download/03102025/mrt-clang-22.0.0git-03102025.tar.gz
+LATEST_URL_XZ=https://github.com/MRT-project/mrt_clang/releases/download/03102025/mrt-clang-22.0.0git-03102025.tar.xz
